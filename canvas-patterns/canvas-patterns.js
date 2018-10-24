ee2f975796eb3dd4ccda8b733fd115fd4a061b8f
Jafar Sadik <dzafar.sadik@gmail.com>
2018-10-24 07:28:15 +0100

    canvas-patterns: fixed render issue

--- canvas-patterns/canvas-patterns.js
+++ canvas-patterns/canvas-patterns.js
@@ -6,7 +6,7 @@ class Colour {
         this.a = a;
     }
 
-    static of(r, g, b, a) {
+    static rgba(r, g, b, a) {
         return new Colour(r, g, b, a);
     }
 }
@@ -28,7 +28,7 @@ class PatternRenderer {
         for (let x = 0; x < this.width; x++) {
             for (let y = 0; y < this.height; y++) {
                 let {r, g, b, a} = this.computePixel(x, y);
-                this.setPixel(r, g, b, a)
+                this.setPixel(x, y, r, g, b, a)
             }
         }
 
@@ -50,37 +50,37 @@ class PatternRenderer {
 
 const pattern1 = new PatternRenderer('pattern1', (x, y) => {
     let c = 50 + Math.sin(x * y) * y;
-    return Colour.of(c, c, c, c);
+    return Colour.rgba(c, c, c, c);
 });
 pattern1.draw();
 
 const pattern2 = new PatternRenderer('pattern2', (x, y) => {
     let c = x * y * Math.tan(100* x * y);
-    return Colour.of(0, 0, 100 + c % 150, 100 + c % 150);
+    return Colour.rgba(0, 0, 100 + c % 150, 100 + c % 150);
 });
 pattern2.draw();
 
 const pattern3 = new PatternRenderer('pattern3', (x, y) => {
     let c = x * y * Math.cos(x + y);
-    return Colour.of(0, 100 + c % 150, 0, c % 255);
+    return Colour.rgba(0, 100 + c % 150, 0, c % 255);
 });
 pattern3.draw();
 
 const pattern4 = new PatternRenderer('pattern4', (x, y) => {
     let c = 10 * Math.sqrt(x * x + y * y);
-    return Colour.of(100 + c % 155, 100 + c % 155, 0, 150);
+    return Colour.rgba(100 + c % 155, 100 + c % 155, 0, 150);
 });
 pattern4.draw();
 
 const pattern5 = new PatternRenderer('pattern5', (x, y) => {
     let c = x * Math.sin(y) * y * Math.cos(x);
-    return Colour.of(c % 255, c % 255, c % 255, 150);
+    return Colour.rgba(c % 255, c % 255, c % 255, 150);
 });
 pattern5.draw();
 
 const pattern6 = new PatternRenderer('pattern6', (x, y) => {
     let c = Math.sqrt(x * Math.sin(y) * y * Math.cos(x));
-    return Colour.of(c % 255, c % 255, c % 255, 150);
+    return Colour.rgba(c % 255, c % 255, c % 255, 150);
 });
 pattern6.draw();
 
@@ -88,7 +88,7 @@ const pattern7 = new PatternRenderer('pattern7', (x, y) => {
     let r = Math.random() * x + y;
     let g = Math.random() * y + x;
     let b = Math.random() * x * y;
-    return Colour.of(r % 250, g % 250, b % 250, 150);
+    return Colour.rgba(r % 250, g % 250, b % 250, 150);
 });
 pattern7.draw();
 
